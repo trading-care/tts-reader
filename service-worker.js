f40ed8ec139c2d6f3c2a5f0aeef0701db11c8ed2
trading-care <tradingcare.email@gmail.com>
2025-04-25 11:08:44 +0700

    Update index.html, fix add service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,31 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('tts-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/icons/icon-192.png',
-          '/icons/icon-512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open('tts-cache').then(cache => {
+//       return cache.addAll([
+//         '/',
+//         '/index.html',
+//         '/manifest.json',
+//         '/icons/icon-192.png',
+//         '/icons/icon-512.png'
+//       ]);
+//     })
+//   );
+// });
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["./", "./index.html", "./icons/icon-192.png"]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
